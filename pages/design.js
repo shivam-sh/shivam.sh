c98e1910bc69b64b970d85a6411c43968632d230
Shivam Sharma <shivam.sharm@icloud.com>
2022-05-27 00:25:27 -0400

    Update fonts and styling for redesign

---
+++ pages/design.js
@@ -0,0 +1,52 @@
+import Head from 'next/head';
+
+import Header from '../components/header';
+import Prompt from '../components/prompt';
+import styles from '../styles/Home.module.scss';
+
+const Design = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Shivam Sh</title>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png"/>
+      </Head>
+
+      <Header />
+
+      <main className={styles.main}>
+        <div className={styles.welcome}>
+          <h1 className={styles.title}>
+            Hey There<span className="accent">,</span>
+            <br />
+            I’m Shivam
+          </h1>
+
+          <caption className={styles.description}>
+            <span className={styles.line}>
+              I’m a Systems Design Engineering
+            </span>
+            <span className={styles.line}>
+              student at the University of Waterloo
+            </span>
+            <span className={styles.line}>
+              I like working on a variety of projects
+            </span>
+            <span className={styles.line}>
+              and sometimes I post about them here
+            </span>
+          </caption>
+        </div>
+      </main>
+
+      <div className={styles.typing}>
+        <p>
+          <span className={styles.bash}>{'> '}</span>
+          <Prompt />
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Design;
