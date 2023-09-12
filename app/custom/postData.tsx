0c1ea4999554eaa90bca533c15627163ce2c4558
Shivam Sh <sharma@shivam.sh>
2023-09-12 04:52:54 +0000

    Clean Up TOC
    - Clean up the Table of Contents for edge cases

--- app/custom/postData.tsx
+++ app/custom/postData.tsx
@@ -82,7 +82,7 @@ export async function fetchWithID(id: string) {
 
 export async function parseTOC(htmlString: string) {
   const $ = load(htmlString);
-  const headings: { id: string; text: string; depth: number }[] = [];
+  let headings: { id: string; text: string; depth: number }[] = [];
 
   $('[id]').each((_, element) => {
     const id = $(element).attr('id') ?? '';
@@ -93,5 +93,17 @@ export async function parseTOC(htmlString: string) {
       headings.push({ id, text, depth });
     }
   });
+
+  // not worth showing if it's a small post
+  if (headings.length < 3) headings = [];
+  
+  // if there are a bunch, only show h1 headings
+  if (headings.length > 5) {
+    headings = headings.filter((entry) => entry.depth == 1);
+  }
+
+  // if there are still a bunch remove the first one (the title)
+  if (headings.length > 5) headings.shift();
+
   return headings;
 }
